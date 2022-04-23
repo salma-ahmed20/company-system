8d1fec80352f843b6180b884c1907af523dc1db5
salma <salma.ahmed0075@gmail.com>
2022-04-24 01:43:09 +0200

    Req #1 - Sales team table filter

--- app/app.module.js
+++ app/app.module.js
@@ -3,11 +3,13 @@ import 'ngVue';
 import 'ngVue/build/plugins.js';
 import PerformancePageComponent from './pages/performance-page.vue';
 import PerformanceChartComponent from './components/vue-components/performance-chart.vue';
+import FilterEmployeesComponent from './components/vue-components/filter-employees.vue';
 
 angular.module('appModule', [
   'ui.router',
   'ngVue',
   'ngVue.plugins',
+  'core',
 ]);
 
 angular.module('appModule').directive('vPerformancePage', (createVueComponent) => {
@@ -16,4 +18,8 @@ angular.module('appModule').directive('vPerformancePage', (createVueComponent) =
 
 angular.module('appModule').directive('vPerformanceChart', (createVueComponent) => {
   return createVueComponent(Vue.component('performanceChartComponent', PerformanceChartComponent));
+});
+
+angular.module('appModule').directive('vFilterEmployees', (createVueComponent) => {
+  return createVueComponent(Vue.component('filterEmployeesComponent', FilterEmployeesComponent));
 });
